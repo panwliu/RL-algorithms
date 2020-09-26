2bad1ad3c60fbe2803a69526447470cb7ecc4351
panwliu <52264766+panwliu@users.noreply.github.com>
2020-09-26 08:32:05 -0700

    add reset() and vis callbacks to mujocosim.cpp

--- src/mujocosim.cpp
+++ src/mujocosim.cpp
@@ -104,9 +104,105 @@ py::array_t<double> MujocoSim::step(py::array_t<double> action)
     return state();
 }
 
+py::array_t<double> MujocoSim::reset(py::array_t<double> qpos_d)
+{
+    int nq = m_->nq;
+
+    py::buffer_info buffer = qpos_d.request();
+    double* buffer_ptr = (double* ) buffer.ptr;
+
+    if (buffer.size == 0)
+    {
+        mj_resetData(m_, d_);
+        mj_forward(m_, d_);
+    }
+    else if (buffer.size != nq)
+    {
+        std::cout<<"State size incorrect! Should be array of size "<<nq<<" ..."<<std::endl;
+    }
+    else
+    {
+        mj_resetData(m_, d_);
+        mj_forward(m_, d_);
+        for (int i=0; i<nq; i++)
+            d_->qpos[i] = buffer_ptr[i];
+        mj_forward(m_, d_);
+    }
+
+    return state();
+}
+
 // ----------- Mujoco Visualizer -----------
+// keyboard callback
+void MujocoVis::keyboard(GLFWwindow* window, int key, int scancode, int act, int mods)
+{
+    // backspace: reset simulation
+    if( act==GLFW_PRESS && key==GLFW_KEY_BACKSPACE )
+    {
+        mj_resetData(this_ptr->m_, this_ptr->d_);
+        mj_forward(this_ptr->m_, this_ptr->d_);
+    }
+}
+
+// mouse button callback
+void MujocoVis::mouse_button(GLFWwindow* window, int button, int act, int mods)
+{
+    // update button state
+    this_ptr->button_left_ =   (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)==GLFW_PRESS);
+    this_ptr->button_middle_ = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)==GLFW_PRESS);
+    this_ptr->button_right_ =  (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)==GLFW_PRESS);
+
+    // update mouse position
+    glfwGetCursorPos(window, &(this_ptr->lastx_), &(this_ptr->lasty_));
+}
+
+// mouse move callback
+void MujocoVis::mouse_move(GLFWwindow* window, double xpos, double ypos)
+{
+    // no buttons down: nothing to do
+    if( !(this_ptr->button_left_) && !(this_ptr->button_middle_) && !(this_ptr->button_right_) )
+        return;
+
+    // compute mouse displacement, save
+    double dx = xpos - this_ptr->lastx_;
+    double dy = ypos - this_ptr->lasty_;
+    this_ptr->lastx_ = xpos;
+    this_ptr->lasty_ = ypos;
+
+    // get current window size
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+
+    // get shift key state
+    bool mod_shift = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT)==GLFW_PRESS ||
+                      glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT)==GLFW_PRESS);
+
+    // determine action based on mouse button
+    mjtMouse action;
+    if( this_ptr->button_right_ )
+        action = mod_shift ? mjMOUSE_MOVE_H : mjMOUSE_MOVE_V;
+    else if( this_ptr->button_left_ )
+        action = mod_shift ? mjMOUSE_ROTATE_H : mjMOUSE_ROTATE_V;
+    else
+        action = mjMOUSE_ZOOM;
+
+    // move camera
+    mjv_moveCamera(this_ptr->m_, action, dx/height, dy/height, &(this_ptr->scn_), &(this_ptr->cam_));
+}
+
+// scroll callback
+void MujocoVis::scroll(GLFWwindow* window, double xoffset, double yoffset)
+{
+    // emulate vertical mouse motion = 5% of window height
+    mjv_moveCamera(this_ptr->m_, mjMOUSE_ZOOM, 0, -0.05*yoffset, &(this_ptr->scn_), &(this_ptr->cam_));
+}
+
+MujocoVis* MujocoVis::this_ptr = NULL;
+
 MujocoVis::MujocoVis(MujocoSim& sim)
 {
+    this_ptr = this;
+
     m_ = sim.model();
     d_ = sim.data();
 
@@ -123,6 +219,11 @@ MujocoVis::MujocoVis(MujocoSim& sim)
 
     mjv_makeScene(m_, &scn_, 1000);
     mjr_makeContext(m_, &con_, mjFONTSCALE_100);
+
+    glfwSetKeyCallback(window_, MujocoVis::keyboard);
+    glfwSetCursorPosCallback(window_, MujocoVis::mouse_move);
+    glfwSetMouseButtonCallback(window_, MujocoVis::mouse_button);
+    glfwSetScrollCallback(window_, MujocoVis::scroll);
 }
 
 bool MujocoVis::window_should_close()
@@ -169,7 +270,8 @@ PYBIND11_MODULE(mujocosim, m) {
         .def("time", &MujocoSim::time)
         .def("state", &MujocoSim::state)
         .def("control", &MujocoSim::control)
-        .def("step", &MujocoSim::step);
+        .def("step", &MujocoSim::step)
+        .def("reset", &MujocoSim::reset);
         
     py::class_<MujocoVis>(m, "MujocoVis")
         .def(py::init<MujocoSim& >())
--- src/mujocosim.h
+++ src/mujocosim.h
@@ -27,6 +27,7 @@ class MujocoSim
         py::array_t<double> state();
         py::array_t<double> control();
         py::array_t<double> step(py::array_t<double> action);
+        py::array_t<double> reset(py::array_t<double> qpos_d);
 
     private:
         mjModel* m_;
@@ -37,7 +38,7 @@ class MujocoVis
 {
     public:
         MujocoVis(MujocoSim& sim);                  // give sim directly will result sim's deconstructor called automatically
-        // MujocoVis(mjModel* _m, mjData* _d);      // python don't work well with pointers, alghough works in c++
+        // MujocoVis(mjModel* _m, mjData* _d);      // python don't work well with pointers
         ~MujocoVis()
         {
             // glfwTerminate();
@@ -46,6 +47,10 @@ class MujocoVis
         }
         bool window_should_close();
         void render();
+        static void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods);
+        static void mouse_button(GLFWwindow* window, int button, int act, int mods);
+        static void mouse_move(GLFWwindow* window, double xpos, double ypos);
+        static void scroll(GLFWwindow* window, double xoffset, double yoffset);
 
     private:
         mjModel* m_;
@@ -55,6 +60,11 @@ class MujocoVis
         mjvScene scn_;                       // abstract scene
         mjrContext con_;
         GLFWwindow* window_;
+
+        bool button_left_ = false, button_middle_ = false, button_right_ =  false;
+        double lastx_=0, lasty_=0;
+
+        static MujocoVis *this_ptr;
 };
 
 
