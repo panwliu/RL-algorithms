56fe66b7a6fc86ae6c5f7fb8dcdc10c3f1745572
panwliu <52264766+panwliu@users.noreply.github.com>
2020-10-30 21:53:01 -0700

    add env walker2d, split sampling buffer, nns out from rl

--- scripts/envs.py
+++ scripts/envs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import mujocosim
 import os, pathlib
+import torch, nns
 
 def env_by_name(env_name):
     file_path = pathlib.Path( os.path.realpath(__file__) )
@@ -12,6 +13,15 @@ def env_by_name(env_name):
     elif env_name == 'CartPole-v1':     # continuous
         model_file = 'cartpole.xml'
         env = CartpoleEnv(model_path = models_path + model_file, discrete=False)
+    elif env_name == 'Walker2D-v0':
+        model_file = 'walker2d.xml'
+        env = Walker2dEnv(model_path = models_path + model_file)
+    elif env_name == 'Walker2D-v1':     # jumping
+        model_file = 'walker2d.xml'
+        env = Walker2dEnv(model_path = models_path + model_file, jumping=True)
+    elif env_name == 'Cassie-v0':
+        model_file = 'cassie.xml'
+        env = CassieEnv(model_path = models_path + model_file)
     else:
         print("Can't find env " + env_name)
     
@@ -22,6 +32,10 @@ class CartpoleEnv:
     def __init__(self, model_path="../models/cartpole.xml", discrete=True):
         self.sim = mujocosim.MujocoSim(model_path)
         self.discrete = discrete
+        if discrete:
+            self.obs_dim, self.act_dim = 4, 2
+        else:
+            self.obs_dim, self.act_dim = 4, 1
 
     def step(self, action):
         if self.discrete:
@@ -63,11 +77,163 @@ class CartpoleEnv:
             self.viewer = mujocosim.MujocoVis(self.sim)
         self.viewer.render()
         
+    def eval(self, path):
+        checkpoint = torch.load(path)
+        args = checkpoint['args']
+        actor = nns.MLPGaussianActor(self.obs_dim, self.act_dim, args.hid, torch.nn.ReLU, 0.3)
+        actor.load_state_dict(checkpoint['actor_state_dict'])
+        actor.eval()
         
+        for _ in range(5):
+            obs = self.reset()
+            for _ in range(1000):
+                self.render()
+                act, _ = actor.action(obs)
+                obs, _, _ = self.step(act)
+        
+class Walker2dEnv:
+    def __init__(self, model_path="../models/walker2d.xml", jumping=False):
+        self.sim = mujocosim.MujocoSim(model_path)
+        self.jumping = jumping
+        if jumping:
+            self.obs_dim, self.act_dim = 17, 3
+        else:
+            self.obs_dim, self.act_dim = 17, 6
+
+    def step(self, action):
+        action = np.clip(action, -1.0, 1.0)
+        if self.jumping:
+            action = np.hstack((action,action))
+            state = self.sim.step(action)
+        else:
+            state = self.sim.step(action)
+
+        height, angle, vel = state[1], state[2], state[9]
+        
+        reward  = 1 + vel
+
+        done = not (height > 0.8 and height < 2.0 and angle > -1.0 and angle < 1.0)
+
+        return state[1:], reward, done
+
+    def reset(self):
+        state = self.sim.reset([])
+        return state[1:]
+
+    def render(self):
+        if not hasattr(self, "viewer"):
+            self.viewer = mujocosim.MujocoVis(self.sim)
+        self.viewer.render()
+
+    def eval(self, path):
+        checkpoint = torch.load(path)
+        args = checkpoint['args']
+        actor = nns.MLPGaussianActor(self.obs_dim, self.act_dim, args.hid, torch.nn.ReLU, 0.2)
+        actor.load_state_dict(checkpoint['actor_state_dict'])
+        actor.eval()
+        
+        for _ in range(5):
+            obs = self.reset()
+            for _ in range(500):
+                self.render()
+                act, _ = actor.action(obs)
+                obs, _, done = self.step(act)
+                if done:
+                    break
+
+
+
+class CassieEnv:
+    def __init__(self, model_path="../models/cassie.xml", reward_type='standing'):
+        self.sim = mujocosim.MujocoSim(model_path)
+        self.obs_dim, self.act_dim = self.cassie_state_fcn(np.zeros(67)).size, 10
+        self.reward_type = reward_type
+
+        self.qpos_init = [0, 0, 1.01, 1, 0, 0, 0,
+        0.0045, 0, 0.4973, 0.9785, -0.0164, 0.01787, -0.2049,
+        -1.1997, 0, 1.4267, 0, -1.5244, 1.5244, -1.5968,
+        -0.0045, 0, 0.4973, 0.9786, 0.00386, -0.01524, -0.2051,
+        -1.1997, 0, 1.4267, 0, -1.5244, 1.5244, -1.5968]
+        self.offset = np.array([0.0045, 0.0, 0.4973, -1.1997, -1.5968, 0.0045, 0.0, 0.4973, -1.1997, -1.5968])
+        self.p_gain = np.array([100,  100,  88,  96,  50,    100,  100,  88,  96,  50])
+        self.d_gain = np.array([10.0, 10.0, 8.0, 9.6, 5.0,   10.0, 10.0, 8.0, 9.6, 5.0])
+
+    def step(self, action):
+        # motor_pos = self.cassie_state[7:17]
+        # motor_vel = self.cassie_state[17:27]
+        # target = action + self.offset
+
+        # action = self.p_gain*(target - motor_pos) + self.d_gain*(0 - motor_vel)
+
+        action_scale = np.array( [4.5, 4.5, 12.2, 12.2, 0.9, 4.5, 4.5, 12.2, 12.2, 0.9] )
+        action = action_scale * action
+
+        state = self.sim.step(action)
+        self.cassie_state = self.cassie_state_fcn(state)
+
+        height, vel = state[2], state[35]
+        
+        if self.reward_type == 'standing':
+            motor_pos = self.cassie_state[7:17]
+            unactuated_joint_pos = self.cassie_state[27:31]
+            left_joint_pos = np.hstack((motor_pos[:5], unactuated_joint_pos[:2]))
+            right_joint_pos = np.hstack((motor_pos[5:], unactuated_joint_pos[2:]))
+            joint_error = np.mean( (left_joint_pos-right_joint_pos)**2 )
+            reward = 1 - joint_error
+        elif self.reward_type == 'forward':
+            reward = 1 + vel
+        else:
+            print('No reward type ' + self.reward_type)
+
+        done = not (height > 0.7 and height < 2.0 )
+
+        return self.cassie_state, reward, done
+
+    def reset(self):
+        state = self.sim.reset(self.qpos_init)
+        self.cassie_state = self.cassie_state_fcn(state)
+        
+        return self.cassie_state
+
+    def render(self):
+        if not hasattr(self, "viewer"):
+            self.viewer = mujocosim.MujocoVis(self.sim)
+        self.viewer.render()
+
+    def cassie_state_fcn(self,state):
+        q_pos, q_vel = state[:35], state[35:]
+        pelvis_orientation = q_pos[3:7]         # size 4
+        pelvis_angularVel = q_vel[3:6]          # size 3
+        motor_pos = [q_pos[7], q_pos[8], q_pos[9], q_pos[14], q_pos[20],   q_pos[21], q_pos[22], q_pos[23], q_pos[28], q_pos[34] ]  # size 10
+        motor_vel = [q_vel[6], q_vel[7], q_vel[8], q_vel[12], q_vel[18],   q_vel[19], q_vel[20], q_vel[21], q_vel[25], q_vel[31] ]  # size 10
+        joint_pos = [q_pos[15], q_pos[16], q_pos[29], q_pos[30] ]   # size 4
+        joint_vel = [q_pos[13], q_pos[14], q_pos[26], q_pos[27] ]   # size 4
+
+        cassie_state = np.hstack((pelvis_orientation, pelvis_angularVel, motor_pos, motor_vel, joint_pos, joint_vel, q_pos[2]))
+
+        return cassie_state
+    
+    def eval(self, path):
+        checkpoint = torch.load(path)
+        args = checkpoint['args']
+        actor = nns.MLPGaussianActor(self.obs_dim, self.act_dim, args.hid, torch.nn.ReLU, 0.3)
+        actor.load_state_dict(checkpoint['actor_state_dict'])
+        actor.eval()
+        
+        for _ in range(10):
+            obs = self.reset()
+            for _ in range(500):
+                self.render()
+                act, _ = actor.action(obs)
+                obs, _, done = self.step(act)
+                if done:
+                    break
+
 
 if __name__=="__main__":
-    # env = env_by_name(env_name="CartPole")
-    env = env_by_name(env_name="CartPole-continuous")
+    # env = env_by_name(env_name="CartPole-v0")
+    # env = env_by_name(env_name="Walker2D-v0")
+    env = env_by_name(env_name="Cassie-v0")
 
     for _ in range(10):
         done = False
@@ -76,7 +242,9 @@ if __name__=="__main__":
         while not done:
             env.render()
 
-            a = np.random.choice(2,1)
+            a = np.zeros(env.act_dim)
+            # a[0] = 1
+            # a[3] = -1
 
             s_, r, done = env.step(a)
 
