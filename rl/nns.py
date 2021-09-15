6e518ddd28bc6b13b5e9aaa02e24ba07a8f8ecbb
panwliu <52264766+panwliu@users.noreply.github.com>
2021-09-15 14:54:45 -0700

    add SAC

--- rl/nns.py
+++ rl/nns.py
@@ -1,5 +1,6 @@
 import torch
-
+import numpy as np
+import torch.nn.functional as F
 
 def mlp(sizes, activation, output_activation=torch.nn.Identity):
     layers = []
@@ -42,3 +43,100 @@ class MLPCritic(torch.nn.Module):
     def forward(self, x):
         return self.v_net(x)
 
+class MLPQFunction(torch.nn.Module):                # for discrete acitons
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+        self.obs_dim = obs_dim
+        self.q_net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
+
+    def forward(self, x):
+        return self.q_net(x)
+    
+    def action(self, x):
+        x = torch.tensor(x, dtype=torch.float32).view(-1,self.obs_dim)
+        q_values = self.forward(x).detach()
+        # Greedy action
+        action = q_values.argmax(dim=1).reshape(-1)
+        return action.numpy()
+
+class MLPQFunction2(torch.nn.Module):               # for continous acitons
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+        self.q_net = mlp([obs_dim+act_dim] + list(hidden_sizes) + [1], activation)
+
+    def forward(self, obs, act):
+        return self.q_net( torch.cat([obs, act], dim=-1) ).squeeze(dim=-1)
+
+class MLPDeterministicActor(torch.nn.Module):      # for continous acitons
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+        self.mu_net = mlp([obs_dim] + list(hidden_sizes) + [act_dim], activation)
+
+    def forward(self, x):
+        return self.mu_net(x)
+
+class MLPActorCritic2(torch.nn.Module):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+
+        self.q1 = MLPQFunction2(obs_dim, act_dim, hidden_sizes, activation)
+        self.q2 = MLPQFunction2(obs_dim, act_dim, hidden_sizes, activation)
+        self.pi = MLPDeterministicActor(obs_dim, act_dim, hidden_sizes, activation)
+
+    def action(self, obs):
+        with torch.no_grad():
+            return self.pi(obs).numpy()
+
+LOG_STD_MAX = 2
+LOG_STD_MIN = -20
+class SquashedGaussianMLPActor(torch.nn.Module):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation, act_limit):
+        super().__init__()
+        self.net = mlp([obs_dim] + list(hidden_sizes), activation, activation)
+        self.mu_layer = torch.nn.Linear(hidden_sizes[-1], act_dim)
+        self.log_std_layer = torch.nn.Linear(hidden_sizes[-1], act_dim)
+        self.act_limit = act_limit
+
+    def forward(self, obs, deterministic=False, with_logprob=True):
+        net_out = self.net(obs)
+        mu = self.mu_layer(net_out)
+        log_std = self.log_std_layer(net_out)
+        log_std = torch.clamp(log_std, LOG_STD_MIN, LOG_STD_MAX)
+        std = torch.exp(log_std)
+
+        # Pre-squash distribution and sample
+        pi_distribution = torch.distributions.Normal(mu, std)
+        if deterministic:
+            # Only used for evaluating policy at test time.
+            pi_action = mu
+        else:
+            pi_action = pi_distribution.rsample()
+
+        if with_logprob:
+            # Compute logprob from Gaussian, and then apply correction for Tanh squashing.
+            # NOTE: The correction formula is a little bit magic. To get an understanding 
+            # of where it comes from, check out the original SAC paper (arXiv 1801.01290) 
+            # and look in appendix C. This is a more numerically-stable equivalent to Eq 21.
+            # Try deriving it yourself as a (very difficult) exercise. :)
+            logp_pi = pi_distribution.log_prob(pi_action).sum(axis=-1)
+            logp_pi -= (2*(np.log(2) - pi_action - F.softplus(-2*pi_action))).sum(axis=1)
+        else:
+            logp_pi = None
+
+        pi_action = torch.tanh(pi_action)
+        pi_action = self.act_limit * pi_action
+
+        return pi_action, logp_pi
+
+class MLPActorCritic3(torch.nn.Module):
+    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
+        super().__init__()
+        # build policy and value functions
+        self.pi = SquashedGaussianMLPActor(obs_dim, act_dim, hidden_sizes, activation, act_limit=1.0)
+        self.q1 = MLPQFunction2(obs_dim, act_dim, hidden_sizes, activation)
+        self.q2 = MLPQFunction2(obs_dim, act_dim, hidden_sizes, activation)
+
+    def act(self, obs, deterministic=False):
+        with torch.no_grad():
+            a, _ = self.pi(obs, deterministic, False)
+            return a.numpy()
---
+++ rl/rlSAC.py
@@ -0,0 +1,192 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+import nns
+import time
+import envs
+import argparse, sys
+import itertools, copy
+
+class ReplayBuffer:
+    def __init__(self, obs_dim, act_dim, buffer_size):
+        self.buffer_size = buffer_size
+        self.obs_buf = np.zeros((buffer_size,obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros((buffer_size,act_dim), dtype=np.float32)
+        self.reward_buf = np.zeros(buffer_size, dtype=np.float32)
+        self.obs_next_buf = np.zeros((buffer_size,obs_dim), dtype=np.float32)
+        self.done_buf = np.zeros(buffer_size, dtype=np.float32)
+        
+        self.ptr, self.buffer_filled = 0, False
+
+    def store(self, obs, act, reward, obs_, done):
+        self.obs_buf[self.ptr,:] = obs
+        self.act_buf[self.ptr,:] = act
+        self.reward_buf[self.ptr] = reward
+        self.obs_next_buf[self.ptr,:] = obs_
+        self.done_buf[self.ptr] = done
+        
+        self.ptr += 1
+        if self.ptr == self.buffer_size:
+            self.ptr = 0
+            self.buffer_filled = True
+    
+    def get(self, batch_size):
+        if self.buffer_filled:
+            idx = np.random.choice(self.buffer_size, batch_size)
+        else:
+            idx = np.random.choice(self.ptr, batch_size)
+        return self.obs_buf[idx,:], self.act_buf[idx,:], self.reward_buf[idx], self.obs_next_buf[idx,:], self.done_buf[idx]
+
+class rlSAC:
+    def __init__(self, ac:nns.MLPActorCritic3, ac_targ:nns.MLPActorCritic3) -> None:
+        self.ac, self.ac_targ = ac, ac_targ
+        self.gamma, self.batch_size, self.alpha = 0.99, 256, 0.2
+
+        for p in self.ac_targ.parameters():
+            p.requires_grad = False
+        
+        self.q_params = itertools.chain(self.ac.q1.parameters(), self.ac.q2.parameters())
+
+        self.optim_pi = torch.optim.Adam(self.ac.pi.parameters(), lr=0.001)
+        self.optim_q = torch.optim.Adam(self.q_params, lr=0.001)
+
+    def action(self, obs, deterministic=False):
+        return self.ac.act(torch.as_tensor(obs, dtype=torch.float32), deterministic)
+
+    def train(self, buffer:ReplayBuffer):
+        obs, acts, rewards, obs_, done = buffer.get(self.batch_size)
+
+        obs = torch.tensor(obs, dtype=torch.float32)
+        acts = torch.tensor(acts, dtype=torch.float32)
+        rewards = torch.tensor(rewards, dtype=torch.float32)
+        obs2 = torch.tensor(obs_, dtype=torch.float32)
+        done = torch.tensor(done, dtype=torch.float32)
+
+        q1 = self.ac.q1(obs,acts)
+        q2 = self.ac.q2(obs,acts)
+
+        with torch.no_grad():
+            # Target actions come from *current* policy
+            acts2, logp_act2 = self.ac.pi(obs2)
+
+            # Target Q-values
+            q1_pi_targ = self.ac_targ.q1(obs2, acts2)
+            q2_pi_targ = self.ac_targ.q2(obs2, acts2)
+            q_pi_targ = torch.min(q1_pi_targ, q2_pi_targ)
+            backup = rewards + self.gamma * (1 - done) * (q_pi_targ - self.alpha * logp_act2)
+
+        self.optim_q.zero_grad()
+        loss_q1 = ((q1 - backup)**2).mean()
+        loss_q2 = ((q2 - backup)**2).mean()
+        loss_q = loss_q1 + loss_q2
+        loss_q.backward()
+        self.optim_q.step()
+
+        for p in self.q_params:
+            p.requires_grad = False
+
+        pi, logp_pi = self.ac.pi(obs)
+        q1_pi = self.ac.q1(obs, pi)
+        q2_pi = self.ac.q2(obs, pi)
+        q_pi = torch.min(q1_pi, q2_pi)
+
+        self.optim_pi.zero_grad()
+        loss_pi = (self.alpha * logp_pi - q_pi).mean()
+        loss_pi.backward()
+        self.optim_pi.step()
+
+        for p in self.q_params:
+            p.requires_grad = True
+
+        polyak = 0.995
+        with torch.no_grad():
+            for p, p_targ in zip(self.ac.parameters(), self.ac_targ.parameters()):
+                # NB: We use an in-place operations "mul_", "add_" to update target
+                # params, as opposed to "mul" and "add", which would make new tensors.
+                p_targ.data.mul_(polyak)
+                p_targ.data.add_((1 - polyak) * p.data)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # # ------------ CartPole ------------
+    # parser.add_argument('--env', type=str, default='CartPole-v1')
+    # parser.add_argument('--memory_size', type=int, default=2000)
+    # parser.add_argument('--hid', type=list, default=[128,64])
+    # parser.add_argument('--lr_a', type=float, default=1e-2)
+    # parser.add_argument('--lr_c', type=float, default=1e-2)
+    # parser.add_argument('--train_a_itrs', type=int, default=20)
+    # parser.add_argument('--train_c_itrs', type=int, default=3)
+    # parser.add_argument('--epochs', type=int, default=100)
+    # parser.add_argument('--save_freq', type=int, default=50)
+    # parser.add_argument('--eval', default=False, action='store_true')
+    # parser.add_argument('--eval_path', type=str, default='')
+    # parser.add_argument('--play_speed', type=int, default=1)
+    # parser.add_argument('--num_procs', type=int, default=1)
+    # ------------ Walker2D-v0/1 ------------
+    parser.add_argument('--env', type=str, default='Walker2D-v1')
+    parser.add_argument('--memory_size', type=int, default=10000)
+    parser.add_argument('--hid', type=list, default=[128,64])
+    parser.add_argument('--lr_a', type=float, default=1e-3)
+    parser.add_argument('--lr_c', type=float, default=1e-2)
+    parser.add_argument('--train_a_itrs', type=int, default=30)
+    parser.add_argument('--train_c_itrs', type=int, default=5)
+    parser.add_argument('--epochs', type=int, default=5000)
+    parser.add_argument('--save_freq', type=int, default=500)
+    parser.add_argument('--eval', default=False, action='store_true')
+    parser.add_argument('--eval_path', type=str, default='')
+    parser.add_argument('--play_speed', type=int, default=1)
+    parser.add_argument('--num_procs', type=int, default=1)
+    args = parser.parse_args()
+
+    torch.manual_seed(0)
+    np.random.seed(0)
+
+    env = envs.env_by_name(args.env)
+
+    ac = nns.MLPActorCritic3(env.obs_dim, env.act_dim, args.hid, torch.nn.ReLU)
+    # ac_targ = nns.MLPActorCritic3(env.obs_dim, env.act_dim, args.hid, torch.nn.ReLU)
+    ac_targ = copy.deepcopy(ac)
+    agent = rlSAC(ac, ac_targ)
+    buffer = ReplayBuffer(env.obs_dim, env.act_dim, args.memory_size)
+
+    total_steps = 500 * 200     # n_episodes * step_per_episodes
+    k_ep, k_ep_step = 0, 0
+    obs = env.reset()
+    for t in range(total_steps):
+        # if k_ep > 300:
+        #     env.render()
+        
+        if t > 5000:
+            act = agent.action(obs)
+        else:
+            act = np.random.uniform(low=-1.0, high=1.0, size=env.act_dim)
+        obs_, reward, done = env.step(act)
+
+        buffer.store(obs, act, reward, obs_, done)
+
+        obs = obs_
+
+        k_ep_step += 1
+        if done:
+            if k_ep % 5 == 0:
+                print(k_ep,"Episode finished after {} timesteps".format(k_ep_step))
+            obs = env.reset()
+            k_ep += 1
+            k_ep_step = 0
+
+        if t > 3000:
+            agent.train(buffer)
+
+    print('--------- test policy ---------')
+    k_ep, k_ep_step = 0, 0
+    obs = env.reset()
+    for t in range(1000):
+        act = agent.action(obs,True)
+        obs, _, done = env.step(act)
+        k_ep_step += 1
+        if done or k_ep_step==999:
+            print(k_ep,"Episode finished after {} timesteps".format(k_ep_step))
+            obs = env.reset()
+            k_ep += 1
+            k_ep_step = 0
